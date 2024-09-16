0eb3e5377b0f38cc046711fcb6380092062673b3
Nikita Ryabinskii <ryabinskiyna@gmail.com>
2024-09-17 01:26:45 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Point point1 = new Point(1, 3);
+        Point point2 = new Point(1, 3);
+        Point point3 = new Point(5, 8);
+
+        System.out.println(point1);
+        System.out.println(point2);
+        System.out.println(point3);
+
+        System.out.println(point1 == point2);
+        System.out.println(point1 == point3);
+        System.out.println(point1.equals(point2));
+        System.out.println(point1.equals(point3));
+    }
+}
